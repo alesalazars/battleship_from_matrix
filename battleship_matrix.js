6f67eda3f641b0328fdb85c1a2dea24198d16624
alejandra salazar <alejandra.salazar@cardumen.cl>
2018-05-24 16:34:15 -0400

    fixed error in coordinates and left blank missed positions

--- battleship_matrix.js
+++ battleship_matrix.js
@@ -5,24 +5,27 @@
 var sea = [];
 var row = [];
 
-for ( i = 0 ; i < 100; i++ ){
+for ( i = 1 ; i < 32; i++ ){
   row.push(0);
 }
-for ( i = 0 ; i < 100 ; i++ ){
+for ( i = 1 ; i < 32 ; i++ ){
   sea.push(row.slice());
 }
 
 // Draw the board to show in the HTML
 var board = document.getElementById('board');
 
-for( i = 0 ; i < sea.length ; i++ ){
+for( i = 1 ; i < sea.length ; i++ ){
   var templateRow = document.createElement('ul');
   board.appendChild(templateRow);
-  for( j = 0 ; j < sea[i].length ; j++ ){
+  for( j = 1 ; j < sea[i].length ; j++ ){
     var templateColumn = document.createElement('li');
+    var templateCoordinate = document.createElement('a');
+    templateColumn.appendChild(templateCoordinate);
     templateRow.appendChild(templateColumn);
-    templateColumn.id = i + "-" + j;
-    templateColumn.className = "boxBlank";
+    templateCoordinate.id = j + "-" + i;
+    templateCoordinate.className = "boxBlank";
+    templateCoordinate.innerHTML = j + "-" + i;
   }
 }
 
@@ -45,19 +48,19 @@ var carrier =[ [28,5], [28,6], [28,7], [28,8], [28,9] ];
 fillWithOnes(carrier);
 
 //Battleship ship
-var battleship =[ [0,3], [1,3], [2,3], [3,3] ];
+var battleship =[ [1,3], [2,3], [3,3], [4,3] ];
 fillWithOnes(battleship);
 
 //Cruiser ship
-var cruiser =[ [43,88], [43,89], [43,90] ];
+var cruiser =[ [8,20], [9,20], [10,20] ];
 fillWithOnes(cruiser);
 
 //Submarine ship
-var submarine =[ [14,0], [14,1], [14,2] ];
+var submarine =[ [14,1], [14,2], [14,3] ];
 fillWithOnes(submarine);
 
 //Destroyer ship
-var destroyer =[ [90,3], [90,4] ];
+var destroyer =[ [24,23], [24,24] ];
 fillWithOnes(destroyer);
 
 
@@ -70,6 +73,7 @@ console.log(sea);
 ////// Clicking on the button runs the function that checks for a coincidence
 
 var button = document.getElementById('submitPositions');
+
 var x;
 var y;
 var ask;
@@ -117,6 +121,8 @@ button.addEventListener('click', function(){
     console.log("You've already sinked this ship, keep trying");
   }else{
     console.log("You've missed :(");
+    var toMiss = document.getElementById(x + "-" + y);
+    toMiss.className = "boxMissed";
   }
 
 });
